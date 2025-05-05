19f95b5a3d60cbcbd460281e023a2aef2b17a8d8
Elena Ivanova <webmaster@sdabg.net>
2025-05-06 02:58:09 +0300

    add public/vendors - for php detection of link for preview (in social media, Viber...) (index.php still not working)

---
+++ public/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phpsandbox/standard',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brunoinds/link-preview-detector' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => 'ecf564929afe378271ae0e24bb51fbd7f5bc664f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../brunoinds/link-preview-detector',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpsandbox/standard' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);

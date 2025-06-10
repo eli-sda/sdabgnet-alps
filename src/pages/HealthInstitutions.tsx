91b6fbcf49af61398af5bcf18ddf88786c6db827
gabyortova <gabi.ortova@gmail.com>
2025-06-10 18:35:25 +0300

    Improved HealthInstitutions page
    - used PageLinkItem for the links
    - add health-institutions.json to store the data for the links

--- src/pages/HealthInstitutions.tsx
+++ src/pages/HealthInstitutions.tsx
@@ -1,35 +1,36 @@
-import { Button } from 'alps-library/atoms/button/Button';
 import { Page } from 'src/organisms/Page';
 import { getTitle } from 'src/utils/Navigation';
 import routes from 'src/routes';
+import { Grid } from 'alps-library/atoms/grids/Grid';
+import { PageLinkItem } from 'src/organisms/PageLinkItem';
+import healthInstitutions from 'src/utils/health-institutions.json';
 
 const breadcrumbsUrls = [routes.health(), routes.health('institutions')];
+
 const HealthInstitutions = () => {
-  const links = [
-    { url: `https://lifeinhope.com`, text: 'Център за здраве в с. Баня' },
-    { url: 'https://healthcare-bg.com/', text: 'Фондация "Грижа за Здравето"' },
-    {
-      url: 'https://www.yanikabg.com/',
-      text: 'Образователен център за деца и младежи с увреден слух "Яника'
-    }
-  ];
+  const title = getTitle(routes.health('institutions'));
+  const links = healthInstitutions;
+
   return (
-    <Page
-      title={getTitle(routes.health('institutions'))}
-      breadcrumbsUrls={breadcrumbsUrls}
-    >
-      {links.map((link, l) => (
-        <Button
-          key={l}
-          as="a"
-          isExternal={true}
-          // href={`${encodeURI(path)}`}
-          url={link.url}
-          label={link.text}
-          simple={true}
-        />
-      ))}
-    </Page>
+    <>
+      <Page title={title} breadcrumbsUrls={breadcrumbsUrls}></Page>
+
+      <Grid
+        className={'l-grid l-grid--7-col l-grid-wrap l-grid-wrap--6-of-7'}
+        seven={true}
+        as="section"
+        wrap={'6'}
+      >
+        {links.map((link, idx) => (
+          <PageLinkItem
+            key={idx}
+            url={link.url}
+            title={link.title}
+            description={link.description}
+          />
+        ))}
+      </Grid>
+    </>
   );
 };
 export default HealthInstitutions;

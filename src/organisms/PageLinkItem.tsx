e3d74b3c9442cb3301af0de6172ee7fbb19a36c8
gabyortova <gabi.ortova@gmail.com>
2025-06-11 11:10:00 +0300

    Add images for links in HealthInstitutions
    - add the option to accept image in PageLinkItem component

--- src/organisms/PageLinkItem.tsx
+++ src/organisms/PageLinkItem.tsx
@@ -1,28 +1,33 @@
 import './PageLinkItem.scss'
 import { GridItem } from 'alps-library/atoms/grids/GridItem';
 import { ContentBlock } from 'src/alps/molecules/blocks/ContentBlock';
+import { ImageType } from 'alps-library/atoms/images/ImageType';
 
 export type PageLinkItemType = {
   url: string;
   title: string;
   description: string;
+  img?: ImageType;
+  sizeAtM?: string;
+  sizeAtXL?: string;
 };
 
-export const PageLinkItem = ({ url, title, description }: PageLinkItemType) => {
+export const PageLinkItem = ({ url, title, description, img, sizeAtM = '3', sizeAtXL = '2' }: PageLinkItemType) => {
   return (
     <>
       <GridItem
         className={
           'u-padding--sides u-space--triple--bottom l-grid-item page-link-item'
         }
-        sizeAtM={'3'}
-        sizeAtXL={'2'}
+        sizeAtM={sizeAtM}
+        sizeAtXL={sizeAtXL}
       >
         <ContentBlock
           title={title}
           description={description}
           url={url}
           cta="Виж повече"
+          image={img || undefined}
         ></ContentBlock>
       </GridItem>
     </>
--- src/pages/HealthInstitutions.tsx
+++ src/pages/HealthInstitutions.tsx
@@ -4,12 +4,20 @@ import routes from 'src/routes';
 import { Grid } from 'alps-library/atoms/grids/Grid';
 import { PageLinkItem } from 'src/organisms/PageLinkItem';
 import healthInstitutions from 'src/utils/health-institutions.json';
+import { ImageType } from 'alps-library/atoms/images/ImageType';
+
+type HealthInstitutionLink = {
+  url: string;
+  title: string;
+  description: string;
+  img: string;
+};
 
 const breadcrumbsUrls = [routes.health(), routes.health('institutions')];
 
 const HealthInstitutions = () => {
   const title = getTitle(routes.health('institutions'));
-  const links = healthInstitutions;
+  const links = healthInstitutions as HealthInstitutionLink[];
 
   return (
     <>
@@ -27,6 +35,19 @@ const HealthInstitutions = () => {
             url={link.url}
             title={link.title}
             description={link.description}
+            img={
+              {
+                alt: link.title,
+                srcSet: {
+                  default: link.img,
+                  500: link.img,
+                  750: link.img,
+                  1200: link.img
+                }
+              } as ImageType
+            }
+            sizeAtM="6"
+            sizeAtXL="3"
           />
         ))}
       </Grid>

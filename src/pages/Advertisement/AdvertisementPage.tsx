d06ccf3f1f5c5a33df882214988b1b4b0d49f99a
gabyortova <gabi.ortova@gmail.com>
2025-06-26 11:19:52 +0300

    Page - add background option AdvertisementPage - add related posts

--- src/pages/Advertisement/AdvertisementPage.tsx
+++ src/pages/Advertisement/AdvertisementPage.tsx
@@ -1,11 +1,42 @@
 import routes from 'src/routes';
-import { AddType } from 'src/constants';
+import { ADD_TYPES, AddType } from 'src/constants';
 import { Page } from 'src/organisms/Page';
 import { getTitle } from 'src/utils/Navigation';
 import AdvertisementForm from './AdvertisementForm';
+import { usePagesMeta } from 'src/hooks/usePagesMeta';
+import { MediaBlockProps } from 'src/alps/molecules/blocks/MediaBlock';
 
 const AdvertisementPage = ({ type }: { type: AddType }) => {
+  const { getMetaMap } = usePagesMeta();
+  const { pageBackground } = usePagesMeta();
   const title = getTitle(routes.advertisement(type));
+  const kicker = getTitle(routes.advertisement());
+
+  const relatedItems: MediaBlockProps[] = [];
+
+  ADD_TYPES.filter((t) => t !== type).forEach((t) => {
+    const url = routes.advertisement(t);
+    const metaMap = getMetaMap([url]);
+    const meta = metaMap[url];
+
+    if (!meta) return;
+
+    const relatedAdverBlock: MediaBlockProps = {
+      title: meta.title,
+      image: {
+        alt: '',
+        srcSet: {
+          default: meta.imageUrl || '',
+          500: '',
+          750: '',
+          1200: ''
+        }
+      },
+      url
+    };
+
+    relatedItems.push(relatedAdverBlock);
+  });
 
   const breadcrumbsUrls = [
     routes.churchLife(),
@@ -17,12 +48,11 @@ const AdvertisementPage = ({ type }: { type: AddType }) => {
     <>
       <Page
         title={title}
+        kicker={kicker}
         breadcrumbsUrls={breadcrumbsUrls}
-        aside={
-          <>
-            <AdvertisementForm type={type} />
-          </>
-        }
+        aside={<AdvertisementForm type={type} />}
+        relatedPosts={{ heading: 'Още обяви', blocks: relatedItems }}
+        background={pageBackground}
       ></Page>
     </>
   );

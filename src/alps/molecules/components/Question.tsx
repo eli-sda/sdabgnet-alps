5c630193a2c1a15c94c8466b82fc8a4943bb77ad
gabyortova <gabi.ortova@gmail.com>
2025-07-07 11:07:54 +0300

    Created component for questions and answers with mockup data

---
+++ src/alps/molecules/components/Question.tsx
@@ -0,0 +1,47 @@
+import { Image } from 'alps-library/atoms/images/Image';
+import { PortableTextBlock } from '@portabletext/types';
+import { CustomPortableText } from 'src/utils/CustomPortableText';
+
+export interface QuestionProps {
+  /**
+   * Specify the avatar of your Comment
+   */
+  avatar: string;
+  name: string;
+  /**
+   * Specify the byline of your Comment
+   */
+  byline: string;
+  text: string | Array<PortableTextBlock>;
+}
+
+export const Question = ({
+  avatar,
+  name,
+  byline,
+  text
+}: QuestionProps): JSX.Element => {
+  return (
+    <div className="c-comment--inner u-border--left u-space--bottom  u-theme--border-color--darker">
+      <div className="c-comment__avatar u-space--right">
+        <Image src={avatar} alt={byline} />
+      </div>
+      <div className="c-comment__body u-spacing--quarter">
+        <div className="c-comment__meta">
+          <span
+            className={
+              'byline u-font--secondary--s can-be--white u-theme--color--darker'
+            }
+          >
+            <p>{name}</p>
+          </span>
+        </div>
+        {typeof text === 'string' ? (
+          <p className="c-comment__content">{text}</p>
+        ) : (
+          <CustomPortableText value={text} />
+        )}
+      </div>
+    </div>
+  );
+};
--- src/pages/PastorOnline.tsx
+++ src/pages/PastorOnline.tsx
@@ -2,6 +2,10 @@ import routes from 'src/routes';
 import { Page } from 'src/organisms/Page';
 import { getTitle } from 'src/utils/Navigation';
 import PastorOnlineForm from './PastorOnlineForm';
+import PastorOnlineQestions from 'src/pages/PastorOnlineQuestions.json';
+import { QuestionItem, Questions } from 'src/alps/organisms/sections/Questions';
+import { PortableTextBlock } from '@portabletext/types';
+import { Caption } from 'alps-library/atoms/text/Caption';
 
 const PastorOnline = () => {
   const title = getTitle(routes.commune('pastor-online'));
@@ -10,12 +14,38 @@ const PastorOnline = () => {
     routes.commune('pastor-online')
   ];
 
+  const items: QuestionItem[] = PastorOnlineQestions.map((item) => ({
+    byline: 'image',
+    text: item.text,
+    avatar: '/img/question_avatar.png',
+    name: item.name,
+    replies: item.answer
+      ? [
+          {
+            byline: 'image',
+            text: item.answer as PortableTextBlock[],
+            avatar: '/img/pastor.jpg',
+            name: 'п-р Венцислав Панайотов'
+          }
+        ]
+      : undefined
+  }));
+
   return (
     <Page
       title={title}
       breadcrumbsUrls={breadcrumbsUrls}
       aside={<PastorOnlineForm />}
-    ></Page>
+    >
+      <Caption>
+        Тук са публикувани някои от зададените въпроси с отговори от{' '}
+        <strong>п-р Венцислав Панайотов</strong>. <br /> Ако имате въпрос, който
+        не откривате тук, свързан с християнския живот, можете да използвате
+        формуляра и ще получите отговор по имейл.
+      </Caption>
+
+      <Questions title="Въпроси" items={items}></Questions>
+    </Page>
   );
 };
 

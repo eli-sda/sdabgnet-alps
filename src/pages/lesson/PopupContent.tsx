b7a114b49436bf8fd1da5cb9b0a97c1bc45f3991
Elena Ivanova <webmaster@sdabg.net>
2025-07-22 00:30:02 +0300

    - AccordionItem CSS styles are moved in alps - Lesson verses by default are from RIBBD - added colors.scss - App.scss - fixed some styles, some styles are moved in Header.scss - added additionalContent property in MediaBlock and use it in LessonQuarterBlock to add PopupContent - open qIntroduction in Dialog when withIntroduction=true

---
+++ src/pages/lesson/PopupContent.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import { Button as AlpsButton } from '../../alps/atoms/Button';
+
+const PopupContent = ({
+  children,
+  title,
+  buttonLabel
+}: {
+  children: React.ReactNode;
+  title?: string;
+  buttonLabel?: string;
+}) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (
+    e: React.MouseEvent<HTMLAnchorElement | HTMLButtonElement | HTMLSpanElement>
+  ) => {
+    e.preventDefault();
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <AlpsButton
+        onClick={handleOpen}
+        label={buttonLabel || 'Прочети повече'}
+        outline={true}
+      />
+
+      <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+        {title && <DialogTitle>{title}</DialogTitle>}
+        <DialogContent>{children}</DialogContent>
+        <DialogActions>
+          <AlpsButton label="Затвори" onClick={handleClose} />
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default PopupContent;

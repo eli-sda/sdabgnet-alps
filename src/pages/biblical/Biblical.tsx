e3c283075963d06d8c01ee27d12c67eaaa27141a
gabyortova <gabi.ortova@gmail.com>
2026-01-27 17:34:19 +0200

    Reorganize constants in Biblical and AudiobooksResources
    - create BaseLinkType in PageLinkItem
    - add links to testimonies-videos.json
    - add AudiobooksResources.scss

--- src/pages/biblical/Biblical.tsx
+++ src/pages/biblical/Biblical.tsx
@@ -1,8 +1,9 @@
 import { useCallback } from 'react';
 import { Page } from 'src/organisms/Page';
 import { Figure } from 'alps-library/molecules/media/figure/Figure';
-import { PageHeaderFeature2 } from 'src/organisms/sections/PageHeaderFeature2';
 import routes from 'src/routes';
+import { RelatedPosts } from 'src/alps/organisms/asides/RelatedPosts';
+import { PageHeaderFeature2 } from 'src/organisms/sections/PageHeaderFeature2';
 import { MAIN_RESOURCES_FOLDER } from 'src/constants';
 import { getImageTypeByUrl } from 'src/utils/ImageHelper';
 import { getTitle } from 'src/utils/Navigation';
@@ -24,58 +25,91 @@ type BelieveSection = {
   items: BelieveItem[];
 };
 
+const izuchavaiMe = [
+  {
+    title: 'Изучаване на Библията',
+    url: 'https://www.bible.izuchavai.me/'
+  },
+  {
+    title: 'Семейството',
+    url: 'https://family.izuchavai.me/'
+  },
+  {
+    title: 'Библейски апокалипсис',
+    url: 'https://otkrovenie.izuchavai.me/'
+  },
+  {
+    title: 'Поеми контрол над здравето си',
+    url: 'https://zdrave.izuchavai.me/'
+  }
+];
+
+const adventisimo = [
+  {
+    title: 'Книгата Даниил',
+    url: 'https://www.adventisimo.com/daniel/'
+  },
+  {
+    title: 'Книгата Откровение',
+    url: 'https://www.adventisimo.com/revelation/'
+  },
+  {
+    title: 'Изследователният съд',
+    url: 'https://www.adventisimo.com/pre-advent-judgment/'
+  },
+  {
+    title: 'Съботата',
+    url: 'https://www.adventisimo.com/sabbath/'
+  },
+  {
+    title: 'Християнски живот',
+    url: 'https://www.adventisimo.com/devotionals/'
+  },
+  {
+    title: 'Елън Уайт',
+    url: 'https://www.adventisimo.com/ellen-white/'
+  }
+];
+
+const audioCourses = (
+  <RelatedPosts
+    heading="Аудио курсове"
+    blocks={[
+      {
+        title: 'Пътуване из Библията',
+        url: '/resources/audio/seminars?playlistTitle=%D0%9F%D1%8A%D1%82%D1%83%D0%B2%D0%B0%D0%BD%D0%B5+%D0%B8%D0%B7+%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D1%8F%D1%82%D0%B0#e39d507d-b8b2-464e-927e-fe5c79f00c64',
+        category: getTitle(routes.resources('audio', 'seminars'))
+      },
+      {
+        title: 'Удивителни факти на Библията',
+        url: '/resources/audio/seminars?playlistTitle=%D0%A3%D0%B4%D0%B8%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D0%BD%D0%B8+%D1%84%D0%B0%D0%BA%D1%82%D0%B8+%D0%BD%D0%B0+%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D1%8F%D1%82%D0%B0#e20275fb-ebd2-4e1b-a82f-7a77979c50f4',
+        category: getTitle(routes.resources('audio', 'seminars'))
+      },
+      {
+        title: 'Основни учения на Библията',
+        url: '/resources/audio/audiobook?playlistTitle=%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D0%B8+%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%8F+%D0%BD%D0%B0+%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D1%8F%D1%82%D0%B0#10181728-18cd-41ac-9b2e-c8dd4ddc4e6e',
+        category: getTitle(routes.resources('audio', 'seminars'))
+      }
+    ]}
+  />
+);
+
+const youTubeLinks = {
+  heading: 'Връзки към YouTube',
+  blocks: [
+    {
+      title: 'Триединството',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi9JXqscXn9D4HZLx-MjRgbX'
+    }
+  ]
+};
+
 const Biblical = () => {
   useScrollToHash();
 
   const breadcrumbsUrls = [routes.info(), routes.info('biblical')];
 
   const believeSections = believe28 as BelieveSection[];
-
-  const izuchavaiMe = [
-    {
-      title: 'Изучаване на Библията',
-      url: 'https://www.bible.izuchavai.me/'
-    },
-    {
-      title: 'Семейството',
-      url: 'https://family.izuchavai.me/'
-    },
-    {
-      title: 'Библейски апокалипсис',
-      url: 'https://otkrovenie.izuchavai.me/'
-    },
-    {
-      title: 'Поеми контрол над здравето си',
-      url: 'https://zdrave.izuchavai.me/'
-    }
-  ];
-
-  const adventisimo = [
-    {
-      title: 'Книгата Даниил',
-      url: 'https://www.adventisimo.com/daniel/'
-    },
-    {
-      title: 'Книгата Откровение',
-      url: 'https://www.adventisimo.com/revelation/'
-    },
-    {
-      title: 'Изследователният съд',
-      url: 'https://www.adventisimo.com/pre-advent-judgment/'
-    },
-    {
-      title: 'Съботата',
-      url: 'https://www.adventisimo.com/sabbath/'
-    },
-    {
-      title: 'Християнски живот',
-      url: 'https://www.adventisimo.com/devotionals/'
-    },
-    {
-      title: 'Елън Уайт',
-      url: 'https://www.adventisimo.com/ellen-white/'
-    }
-  ];
 
   const subLinks = useCallback(
     (links: { url: string; title: string }[]) =>
@@ -96,32 +130,15 @@ const Biblical = () => {
       )),
     []
   );
+
   return (
     <>
       <Page
         pageClassName="biblical"
         title={getTitle(routes.info('biblical'))}
         breadcrumbsUrls={breadcrumbsUrls}
-        relatedPosts={{
-          heading: 'Аудио курсове',
-          blocks: [
-            {
-              title: 'Пътуване из Библията',
-              url: '/resources/audio/seminars?playlistTitle=%D0%9F%D1%8A%D1%82%D1%83%D0%B2%D0%B0%D0%BD%D0%B5+%D0%B8%D0%B7+%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D1%8F%D1%82%D0%B0#e39d507d-b8b2-464e-927e-fe5c79f00c64',
-              category: getTitle(routes.resources('audio', 'seminars'))
-            },
-            {
-              title: 'Удивителни факти на Библията',
-              url: '/resources/audio/seminars?playlistTitle=%D0%A3%D0%B4%D0%B8%D0%B2%D0%B8%D1%82%D0%B5%D0%BB%D0%BD%D0%B8+%D1%84%D0%B0%D0%BA%D1%82%D0%B8+%D0%BD%D0%B0+%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D1%8F%D1%82%D0%B0#e20275fb-ebd2-4e1b-a82f-7a77979c50f4',
-              category: getTitle(routes.resources('audio', 'seminars'))
-            },
-            {
-              title: 'Основни учения на Библията',
-              url: '/resources/audio/audiobook?playlistTitle=%D0%9E%D1%81%D0%BD%D0%BE%D0%B2%D0%BD%D0%B8+%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D1%8F+%D0%BD%D0%B0+%D0%91%D0%B8%D0%B1%D0%BB%D0%B8%D1%8F%D1%82%D0%B0#10181728-18cd-41ac-9b2e-c8dd4ddc4e6e',
-              category: getTitle(routes.resources('audio', 'seminars'))
-            }
-          ]
-        }}
+        aside={audioCourses}
+        relatedPosts={youTubeLinks}
       >
         <section className="u-clear-fix">
           <Figure
--- src/pages/resources/audio/audiobooks/AudioBooksResources.tsx
+++ src/pages/resources/audio/audiobooks/AudioBooksResources.tsx
@@ -1,23 +1,86 @@
 import { useMemo } from 'react';
 import { HeadingBlock } from 'alps-library/molecules/blocks/headingBlock/HeadingBlock';
 import { Figure } from 'alps-library/molecules/media/figure/Figure';
-import { RelatedPostsProps } from 'src/alps/organisms/asides/RelatedPosts';
+import { RelatedPosts } from 'src/alps/organisms/asides/RelatedPosts';
+import { BaseLinkType } from 'src/organisms/PageLinkItem';
 import audiobooksForChildrenData from './audiobooksForChildren.json';
 import AudioPage from '../AudioPage';
+import './AudiobooksResources.scss';
 
-type audiobooksForChildrenType = {
-  title: string;
-  url: string;
+const youTubeAudiobooks = {
+  heading: 'Аудииокниги в YouTube',
+  blocks: [
+    {
+      title: 'Елън Уайт - Свидетелства към църквата - том 1',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi8x41Z2FSkoIuBVhZD8i_ce'
+    },
+    {
+      title: 'Елън Уайт - Свидетелства към църквата - том 2',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi_o1Ll7xE6no5489C3xPl9Y'
+    },
+    {
+      title: 'Елън Уайт - Свидетелства към църквата - том 3',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi-y7lThJ7GUKZ9M3CdQ1vRI'
+    },
+    {
+      title: 'СВИДЕТЕЛСТВА КЪМ ЦЪРКВАТА ТОМ 4',
+      url: 'https://www.youtube.com/playlist?list=PLqis216kTTZmp0lAa3ryrzUWwjspxQ02R'
+    },
+    {
+      title: 'Елън Уайт - Свидетелства към църквата - том 5',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi_HlIiKEJ2AWSI4BzRspiHu'
+    },
+    {
+      title: 'СВИДЕТЕЛСТВА КЪМ ЦЪРКВАТА ТОМ 6',
+      url: 'https://www.youtube.com/playlist?list=PLqis216kTTZnFnH-AQYfbcrYGL4p7AWs-'
+    },
+    {
+      title: 'Том 7 - Свидетелства към църквата',
+      url: 'https://www.youtube.com/playlist?list=PLENr8lV9VWpd1AJYWYcq0RX0hQ5WPwwiL'
+    },
+    {
+      title: 'Елън Уайт - Свидетелства към църквата - том 8',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi_0ZTS0MrtyV-uK5G64lVBB'
+    },
+    {
+      title: 'Елън Уайт - Свидетелства към църквата - том 9',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi-cL1PXwq2fYdlC9d9aXEjw'
+    },
+    {
+      title: 'Елън Уайт - Евангелизъм - книга 1',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi8QDRtxayIqXQbtkOePflQE'
+    },
+    {
+      title: 'Елън Уайт - Евангелизъм - книга 2',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi9LJ7YISoRlqQf3whFTNe5l'
+    },
+    {
+      title: 'Елън Уайт - Евангелизъм - книга 3',
+      url: 'https://www.youtube.com/playlist?list=PLVYPzB4Uygi_P4nqJtjMsBGRmCmNL5yuU'
+    }
+  ]
 };
 
+const relatedAudio = (
+  <RelatedPosts
+    heading="Други аудиокниги"
+    blocks={[
+      {
+        title: 'Срещи с Христос',
+        url: 'https://3-16.bg/sreshti-s-hristos/',
+        category: '3-16.br'
+      }
+    ]}
+  />
+);
+
 const AudiobooksResources = () => {
-  const audiobooksForChildren: audiobooksForChildrenType[] =
-    audiobooksForChildrenData;
+  const audiobooksForChildren: BaseLinkType[] = audiobooksForChildrenData;
 
   // Create aside content with audiobooksForChildren
   const asideContent = useMemo(
     () => (
-      <>
+      <section className="asside-section u-spacing">
         {audiobooksForChildren.length > 0 && (
           <div>
             <div className="u-space--bottom">
@@ -37,27 +100,18 @@ const AudiobooksResources = () => {
             })}
           </div>
         )}
-      </>
+
+        {relatedAudio}
+      </section>
     ),
     [audiobooksForChildren]
   );
-
-  const relatedAudio: RelatedPostsProps = {
-    heading: 'Други аудиокниги',
-    blocks: [
-      {
-        title: 'Срещи с Христос',
-        url: 'https://3-16.bg/sreshti-s-hristos/',
-        category: '3-16.br'
-      }
-    ]
-  };
 
   return (
     <AudioPage
       type="audiobook"
       aside={asideContent ?? undefined}
-      relatedPosts={relatedAudio}
+      relatedPosts={youTubeAudiobooks}
     />
   );
 };

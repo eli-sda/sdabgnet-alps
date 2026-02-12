1ba14a3dc9f57ba76be71b2c4fb34b999ff67dee
gabyortova <gabi.ortova@gmail.com>
2026-02-12 12:59:15 +0200

    Fixed PR #181
    - rename "Проекта SEED" to "Проектът SEED" in Navigation.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Sabbath } from 'alps-library/organisms/asides/sabbath/Sabbath';
 import { Main } from 'alps-library/templates/Main';
-import { ResourceUnavailableNotice } from 'src/components/ResourceUnavailableNotice';
+import { ResourceUnavailableNotice } from 'src/components/resourceNotification/ResourceUnavailableNotice';
 import Footer from './Footer';
 import Header from './Header';
 

4cf0b99d681df4a2a6067db39ca2052ff79d48bc
gabyortova <gabi.ortova@gmail.com>
2026-02-23 14:51:03 +0200

    Add caption

--- src/pages/feedback/Feedback.tsx
+++ src/pages/feedback/Feedback.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Caption } from 'alps-library/atoms/text/Caption';
 import routes from 'src/routes';
 import { Page } from 'src/organisms/Page';
 import { getTitle } from 'src/utils/Navigation';
@@ -31,7 +33,16 @@ const Feedback = () => {
       breadcrumbsUrls={breadcrumbsUrls}
     >
       <section className="feedback-page">
-        <ul className="u-spacing--double">
+        <Caption>
+          За да изпратите отзив за обновения сайт на Адвентната българска мреж@,
+          използвайте <NavLink to={routes.contact}>формата за контакт</NavLink>{' '}
+          или пишете имейл на{' '}
+          <a href="mailto:webmaster@sdabg.net?subject=Отзив за sdabg.net">
+            webmaster@sdabg.net
+          </a>
+        </Caption>
+
+        <ul className="u-spacing--double u-space--double--top">
           {feedback.map((item, index) => (
             <li key={index} className="u-theme--color--darker">
               <i className="far fa-comment u-space--half--right"></i>
@@ -39,7 +50,7 @@ const Feedback = () => {
               <br />
               <div className="u-space--half--top info">
                 <span>{item.name}</span>
-                <span className='u-space--half--left'>{item.date}</span>
+                <span className="u-space--half--left">{item.date}</span>
               </div>
               <hr className="u-background-color--winter u-space--half--top" />
             </li>

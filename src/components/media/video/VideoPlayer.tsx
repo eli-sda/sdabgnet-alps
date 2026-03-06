4e8062f5a376a5da98d4f245c043d435b8b7a687
gabyortova <gabi.ortova@gmail.com>
2026-03-06 16:29:43 +0200

    Use react-resizable-panels in video player

--- src/components/media/video/VideoPlayer.tsx
+++ src/components/media/video/VideoPlayer.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
+import { Panel, Group, Separator } from 'react-resizable-panels';
 import { HeadingBlock } from 'alps-library/molecules/blocks/headingBlock/HeadingBlock';
 import { Figure } from 'alps-library/molecules/media/figure/Figure';
 import { Caption } from 'alps-library/atoms/text/Caption';
@@ -48,51 +49,62 @@ const VideoPlayer = ({ playlist, isVisible = true }: VideoPlayerProps) => {
   return (
     <div className="videoPlayer u-spacing">
       <HeadingBlock title={playlistTitle} />
+
       {playlistAuthor && (
         <h3 className="u-font--secondary--m u-theme--color--darker u-space--half--top">
           {playlistAuthor}
         </h3>
       )}
 
-      <div className="videoPlayer-layout">
-        <div className="videoPlayer-layout-player">
-          {currentVideo ? (
-            <Figure
-              caption={`${currentVideo.title}\n\n${
-                currentVideo.description || ''
-              }`}
-              size="large"
-              videoSrc={`https://www.youtube.com/embed/${currentVideo.videoId}?autoplay=1`}
-              onVideoEnded={handleVideoEnded}
-              isVisible={isVisible}
-            />
-          ) : (
-            <Caption>Няма налично видео</Caption>
-          )}
-        </div>
-        <div className="videoPlayer-layout-sidebar u-border--left u-theme--border-color--darker">
-          {videoItems.map((video, i) => {
-            const thumb = `https://img.youtube.com/vi/${video.videoId}/hqdefault.jpg`;
-            const isActive = currentIndex === i;
+      <Group orientation="horizontal" className="videoPlayer-layout">
+        {/* VIDEO PANEL */}
+        <Panel defaultSize={70} minSize={40}>
+          <div className="videoPlayer-layout-player">
+            {currentVideo ? (
+              <Figure
+                caption={`${currentVideo.title}\n\n${currentVideo.description || ''}`}
+                size="large"
+                videoSrc={`https://www.youtube.com/embed/${currentVideo.videoId}?autoplay=1`}
+                onVideoEnded={handleVideoEnded}
+                isVisible={isVisible}
+              />
+            ) : (
+              <Caption>Няма налично видео</Caption>
+            )}
+          </div>
+        </Panel>
 
-            return (
-              <div
-                key={i}
-                className={`videoItem ${isActive ? 'active' : ''}`}
-                onClick={() => playVideo(i)}
-              >
-                <div className="video-index-container">
-                  <span className="u-font--secondary--xs">
-                    {isActive ? <i className="fas fa-play"></i> : i + 1}
-                  </span>
+        {/* RESIZE HANDLE */}
+        <Separator className="videoPlayer-divider" />
+
+        {/* PLAYLIST PANEL */}
+        <Panel defaultSize={30} minSize={20}>
+          <div className="videoPlayer-layout-sidebar u-border--left u-theme--border-color--darker">
+            {videoItems.map((video, i) => {
+              const thumb = `https://img.youtube.com/vi/${video.videoId}/hqdefault.jpg`;
+              const isActive = currentIndex === i;
+
+              return (
+                <div
+                  key={i}
+                  className={`videoItem ${isActive ? 'active' : ''}`}
+                  onClick={() => playVideo(i)}
+                >
+                  <div className="video-index-container">
+                    <span className="u-font--secondary--xs">
+                      {isActive ? <i className="fas fa-play"></i> : i + 1}
+                    </span>
+                  </div>
+
+                  <img src={thumb} />
+
+                  <h4 className="videoItem-text hyphens-auto">{video.title}</h4>
                 </div>
-                <img src={thumb} />
-                <h4 className="videoItem-text hyphens-auto">{video.title}</h4>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+              );
+            })}
+          </div>
+        </Panel>
+      </Group>
     </div>
   );
 };

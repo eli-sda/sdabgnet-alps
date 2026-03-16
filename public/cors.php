9ee571b37613756796e1b7bf0448355dc207084c
Elena Ivanova <webmaster@sdabg.net>
2026-03-16 17:03:01 +0200

    Don't allow POST requests from localhost
    - updated cors.php
    - Redirect HTTP to HTTPS
    
    https://app.asana.com/1/1206475455470355/project/1209411132678011/task/1213665155237038

--- public/cors.php
+++ public/cors.php
@@ -1,11 +1,19 @@
 <?php
 include_once __DIR__ . '/constants.php';
 $allowed_origins = [
-    'http://localhost:5173',  // React dev server
+    // 'http://localhost:5173',  // React dev server
     $site
 ];
 
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // If the request includes an Origin header, require it to be in the allow list
+    if (!in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins, true)) {
+        http_response_code(403);
+        header('Content-Type: text/plain; charset=UTF-8');
+        echo 'Origin not allowed';
+        exit(0);
+    }
+
     header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
 }
 
@@ -20,6 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405); // Method Not Allowed
     header('Allow: POST, OPTIONS');
+    header('Content-Type: text/plain; charset=UTF-8');
     echo 'Method Not Allowed';
-    exit;
+    exit(0);
 }

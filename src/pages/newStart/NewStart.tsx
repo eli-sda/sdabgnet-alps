a6a385651203a83c27e035cb5622884e451440d9
Elena Ivanova <webmaster@sdabg.net>
2026-04-29 23:40:11 +0300

    New Start - single space after <PageHeaderFeature2>

--- src/pages/newStart/NewStart.tsx
+++ src/pages/newStart/NewStart.tsx
@@ -241,7 +241,7 @@ const NewStart = (): JSX.Element => {
         </section>
       </Page>
 
-      <section className="new-start-videos u-space--top u-spacing--double">
+      <section className="new-start-videos u-space--top u-spacing">
         <PageHeaderFeature2
           blockType="quarterSS"
           blocks={[

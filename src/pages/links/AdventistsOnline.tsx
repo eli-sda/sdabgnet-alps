dd2931043fde3eb8e4ce0b84b9e546d04aefe40f
gabyortova <gabi.ortova@gmail.com>
2026-05-20 17:31:04 +0300

    Add search to AdventistsOnline
    - update filterSectionedData to support primitive type validation and parent-level matching
    - integrate BaseSearch into AdventistsOnline component using title and description keys
    - include children prop support in MediaLinksPageProps interface

--- src/pages/links/AdventistsOnline.tsx
+++ src/pages/links/AdventistsOnline.tsx
@@ -1,25 +1,33 @@
 import { useEffect, useState } from 'react';
-import MediaLinksPage, { LinkGroup } from './MediaLinksPage';
-import { getTitle } from 'src/utils/Navigation';
+import { BaseSearch } from 'alps-library/molecules/forms/elements/BaseSearch';
+import { Caption } from 'alps-library/atoms/text/Caption';
 import routes from 'src/routes';
+import { getTitle } from 'src/utils/Navigation';
+import { filterSectionedData } from 'src/utils/filterHelpers';
+import MediaLinksPage, { LinkGroup } from './MediaLinksPage';
 
 const AdventistsOnline = (): JSX.Element => {
   const [bgLinks, setBgLinks] = useState<LinkGroup[]>([]);
+  const [filteredLinks, setFilteredLinks] = useState<LinkGroup[]>([]);
 
   useEffect(() => {
     fetch('/json/adventists-online.json')
       .then((res) => res.json())
-      .then((data: LinkGroup[]) => setBgLinks(data))
+      .then((data: LinkGroup[]) => {
+        setBgLinks(data);
+        setFilteredLinks(data);
+      })
       .catch((err) => {
         console.error('Failed to load adventists-online.json', err);
         setBgLinks([]);
+        setFilteredLinks([]);
       });
   }, []);
 
   return (
     <MediaLinksPage
       mediaType="bg-links"
-      linksJson={bgLinks}
+      linksJson={filteredLinks}
       relatedPosts={{
         heading: 'Други връзки',
         blocks: [
@@ -40,7 +48,25 @@ const AdventistsOnline = (): JSX.Element => {
           }
         ]
       }}
-    />
+    >
+      <div>
+        <BaseSearch
+          placeholder="Търси по заглавие или описание"
+          hideSearchButton
+          onSearch={(e: React.ChangeEvent<HTMLInputElement>) => {
+            const v = e.target.value;
+            setFilteredLinks(
+              filterSectionedData(bgLinks, v, ['title', 'description'])
+            );
+          }}
+          onSubmit={() => false}
+        />
+
+        {bgLinks.length > 0 && filteredLinks.length === 0 && (
+          <Caption>Няма намерени резултати.</Caption>
+        )}
+      </div>
+    </MediaLinksPage>
   );
 };
 

c28e8b047f953078a5f9453285cc97a6aac155dc
gabyortova <gabi.ortova@gmail.com>
2026-06-05 17:45:55 +0300

    Add button on the homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,7 +11,7 @@ import { Partners } from 'src/components/Partners';
 import { CallForHelp } from 'src/components/CallForHelp';
 
 // for Sanity update
-// import { Button } from '@mui/material';
+import { Button } from '@mui/material';
 // import { deleteAllLinks } from 'src/utils/DelteSanityDocuments';
 // import { fixSlugs } from 'src/utils/Sanity/fix-slug';
 // import {
@@ -24,6 +24,7 @@ import { CallForHelp } from 'src/components/CallForHelp';
 // } from 'src/utils/Sanity/link-playlists';
 // import { deleteAllAudioLinks } from 'src/utils/Sanity/delete-audio-links';
 // import { cleanupPlaylistItems } from 'src/utils/Sanity/cleanup-playlist-items';
+import { updateVideoTopics } from 'src/utils/Sanity/updateVideoTopics';
 
 // for DEMO:
 // import { TestLinks } from 'src/components/TestLinks';
@@ -134,6 +135,11 @@ const Home = () => {
           Fill Health Playlists
         </Button>
       </div> */}
+      <div>
+        <Button onClick={void updateVideoTopics()}>
+          Update video topics
+        </Button>
+      </div>
 
       <section className="full-page u-spacing--triple">
         {/* <VideoDemo /> */}

9d9a19d6ffdd5a113adcecac704606d8b1f511d9
gabyortova <gabi.ortova@gmail.com>
2026-06-05 12:52:53 +0300

    Fix PR #273

--- src/pages/health/healthBooks/HealthBooks.tsx
+++ src/pages/health/healthBooks/HealthBooks.tsx
@@ -1,4 +1,5 @@
 import routes from 'src/routes';
+import { HeadingBlock } from 'alps-library/molecules/blocks/headingBlock/HeadingBlock';
 import { ContentBlock } from 'src/alps/molecules/blocks/ContentBlock';
 import { Button } from 'src/alps/atoms/Button';
 import { Page } from 'src/organisms/Page';
@@ -7,21 +8,24 @@ import { getImageTypeByUrl } from 'src/utils/ImageHelper';
 import VideoPlaylistList from 'src/components/media/video/VideoPlaylistList';
 import './HealthBooks.scss';
 
-const booksBlocks = [
+const onlineBooksBlocks = [
   {
     title: 'Диета и храна',
     url: 'https://m.egwwritings.org/bg/book/13791.1',
     img: '/img/health/books/dieta-i-hrana.webp',
     description: 'Елън Уайт',
-    cta: 'Прочети'
+    cta: 'Чети онлайн'
   },
   {
     title: 'По стъпките на Великия лекар',
     url: 'https://m.egwwritings.org/bg/book/13845.3',
     img: '/img/health/books/po-stupkite-na-velikiya-lekar.webp',
     description: 'Елън Уайт',
-    cta: 'Прочети'
-  },
+    cta: 'Чети онлайн'
+  }
+];
+
+const buyBooksBlocks = [
   {
     title: 'Как да сме здрави?',
     url: 'https://www.book.store.bg/p593363027/kak-da-sme-zdravi-delian-jordanov.html',
@@ -32,27 +36,27 @@ const booksBlocks = [
 ];
 
 const relatedBooks = {
-  heading: 'Още книги за здравето',
+  heading: 'Книги за изтегляне',
   blocks: [
     {
       title: 'Съвети за диетата и храненето',
-      url: '/resources/books?title=%D0%A1%D1%8A%D0%B2%D0%B5%D1%82%D0%B8+%D0%B7%D0%B0+%D0%B4%D0%B8%D0%B5%D1%82%D0%B0%D1%82%D0%B0+%D0%B8+%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B5%D1%82%D0%BE#4eba2f1c-729e-45c0-b56f-e574004bd554',
+      url: '/resources/books#4eba2f1c-729e-45c0-b56f-e574004bd554',
       category: 'Ресурси - книги'
     },
     {
       title: 'По стъпките на Великия лекар',
-      url: '/resources/books?title=%D0%9F%D0%BE+%D1%81%D1%82%D1%8A%D0%BF%D0%BA%D0%B8%D1%82%D0%B5+%D0%BD%D0%B0+%D0%92%D0%B5%D0%BB%D0%B8%D0%BA%D0%B8%D1%8F+%D0%BB%D0%B5%D0%BA%D0%B0%D1%80#f4cf45eb-777b-4f67-a6db-5ac03d7ccd1f',
+      url: '/resources/books#f4cf45eb-777b-4f67-a6db-5ac03d7ccd1f',
       category: 'Ресурси - книги'
     },
     {
       title: 'Децата, дрогата, родителите',
-      url: '/resources/books?title=%D0%94%D0%B5%D1%86%D0%B0%D1%82%D0%B0%2C+%D0%B4%D1%80%D0%BE%D0%B3%D0%B0%D1%82%D0%B0%2C+%D1%80%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D0%B8%D1%82%D0%B5#0b200b72-98c7-45a4-a1d1-ab3b5dedc795',
+      url: '/resources/books#0b200b72-98c7-45a4-a1d1-ab3b5dedc795',
       category: 'Ресурси - книги'
     },
     {
       title:
         'Горчивите методи на лечение. Пътищата на заблудата в алтернативните методи на лечение',
-      url: '/resources/books?title=%D0%93%D0%BE%D1%80%D1%87%D0%B8%D0%B2%D0%B8%D1%82%D0%B5+%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B8+%D0%BD%D0%B0+%D0%BB%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B5.+%D0%9F%D1%8A%D1%82%D0%B8%D1%89%D0%B0%D1%82%D0%B0+%D0%BD%D0%B0+%D0%B7%D0%B0%D0%B1%D0%BB%D1%83%D0%B4%D0%B0%D1%82%D0%B0+%D0%B2+%D0%B0%D0%BB%D1%82%D0%B5%D1%80%D0%BD%D0%B0%D1%82%D0%B8%D0%B2%D0%BD%D0%B8%D1%82%D0%B5+%D0%BC%D0%B5%D1%82%D0%BE%D0%B4%D0%B8+%D0%BD%D0%B0+%D0%BB%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B5#0d0e271b-3e34-49d6-8ae8-e1cedaecbcce',
+      url: '/resources/books#0d0e271b-3e34-49d6-8ae8-e1cedaecbcce',
       category: 'Ресурси - книги'
     }
   ]
@@ -70,20 +74,41 @@ const HealthBooks = (): JSX.Element => {
       relatedPosts={relatedBooks}
     >
       <section className="health-books-page u-spacing--double">
-        <VideoPlaylistList pagePath={healthBooksPath} />
+        <section className="u-spacing">
+          <HeadingBlock title="Гледайте" />
+          <VideoPlaylistList pagePath={healthBooksPath} />
+        </section>
 
-        <section className="u-spacing health-books-list">
-          {booksBlocks.map((book) => (
-            <ContentBlock
-              key={book.url}
-              title={book.title}
-              cta={book.cta}
-              url={book.url}
-              image={getImageTypeByUrl(book.img)}
-              grayBackground={false}
-              description={book.description}
-            />
-          ))}
+        <section className="u-spacing--double health-books-list">
+          <div className="u-spacing">
+            <HeadingBlock title="Четете онлайн" />
+            {onlineBooksBlocks.map((book) => (
+              <ContentBlock
+                key={book.url}
+                title={book.title}
+                cta={book.cta}
+                url={book.url}
+                image={getImageTypeByUrl(book.img)}
+                grayBackground={false}
+                description={book.description}
+              />
+            ))}
+          </div>
+
+          <div className="u-spacing">
+            <HeadingBlock title="Купи книга" />
+            {buyBooksBlocks.map((book) => (
+              <ContentBlock
+                key={book.url}
+                title={book.title}
+                cta={book.cta}
+                url={book.url}
+                image={getImageTypeByUrl(book.img)}
+                grayBackground={false}
+                description={book.description}
+              />
+            ))}
+          </div>
         </section>
 
         <section className="new-life-ad u-spacing">

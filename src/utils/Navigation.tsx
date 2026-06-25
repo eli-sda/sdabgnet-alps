09532bf540a45e69be0176d5bf29c07e50096407
gabyortova <gabi.ortova@gmail.com>
2026-06-25 15:15:35 +0300

    Remove 'Проповеди, Статии' and 'Хумор' from the navigation

--- src/utils/Navigation.tsx
+++ src/utils/Navigation.tsx
@@ -106,12 +106,12 @@ export const primaryNavigationItems: PrimaryNavItemProps[] = [
           }
         ]
       },
-      {
-        type: 'primary',
-        text: 'Проповеди, Статии',
-        url: routes.churchLife('topics'),
-        isDisabled: true
-      },
+      // {
+      //   type: 'primary',
+      //   text: 'Проповеди, Статии',
+      //   url: routes.churchLife('topics'),
+      //   isDisabled: true
+      // },
       {
         type: 'primary',
         text: 'Поезия',
@@ -152,12 +152,12 @@ export const primaryNavigationItems: PrimaryNavItemProps[] = [
         url: routes.churchLife('testimonies'),
         faIconClass: 'fas fa-globe'
       },
-      {
-        type: 'primary',
-        text: 'Хумор',
-        url: routes.churchLife('humor'),
-        isDisabled: true
-      },
+      // {
+      //   type: 'primary',
+      //   text: 'Хумор',
+      //   url: routes.churchLife('humor'),
+      //   isDisabled: true
+      // },
       {
         type: 'primary',
         text: 'Дарения за каузи',
